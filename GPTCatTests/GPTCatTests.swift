9206682400e6d4ee25e0265062d89ffbe34b65aa
Ivan Sergeyenko <svintus@gmail.com>
2025-10-06 23:35:57 -0700

    Removed autogenerated tests, they were not very useful

--- GPTCatTests/GPTCatTests.swift
+++ GPTCatTests/GPTCatTests.swift
@@ -10,8 +10,5 @@ import Testing
 
 struct GPTCatTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
+    // TODO
 }
